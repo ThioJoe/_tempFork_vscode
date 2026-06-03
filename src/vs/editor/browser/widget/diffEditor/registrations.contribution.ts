2a43f5f50b4f287d54b7dce160b7ea9d3b102a8c
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2026-06-03 11:44:02 -0700

    Add diff move color background variables

--- src/vs/editor/browser/widget/diffEditor/registrations.contribution.ts
+++ src/vs/editor/browser/widget/diffEditor/registrations.contribution.ts
@@ -22,6 +22,20 @@ export const diffMoveBorderActive = registerColor(
 	localize('diffEditor.moveActive.border', 'The active border color for text that got moved in the diff editor.')
 );
 
+export const diffMoveBackground = registerColor(
+	'diffEditor.move.background',
+	{ dark: '#8b8b8b33', light: '#8b8b8b33', hcDark: null, hcLight: null },
+	localize('diffEditor.move.background', 'Background color for text that got moved in the diff editor. The color must not be opaque so as not to hide underlying decorations.'),
+	true
+);
+
+export const diffMoveBackgroundActive = registerColor(
+	'diffEditor.moveActive.background',
+	{ dark: '#FFA50033', light: '#FFA50033', hcDark: null, hcLight: null },
+	localize('diffEditor.moveActive.background', 'The active background color for text that got moved in the diff editor. The color must not be opaque so as not to hide underlying decorations.'),
+	true
+);
+
 export const diffEditorUnchangedRegionShadow = registerColor(
 	'diffEditor.unchangedRegionShadow',
 	{ dark: '#000000', light: '#737373BF', hcDark: '#000000', hcLight: '#737373BF', },
